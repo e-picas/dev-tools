d3bd7d097e6dadb280a10ab03f1a60838d2f469d
Piero Wbmstr <me@e-piwi.fr>
2014-04-01 08:22:15 +0200

    License infos in scripts

--- devtools-actions/md2man.sh
+++ devtools-actions/md2man.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 # 
-# DevTools - Packages development & deployment facilities
-# Copyleft (c) 2013 Pierre Cassat and contributors
+# Dev-Tools - Packages development & deployment facilities
+# Copyleft (C) 2013-2014 Pierre Cassat & contributors
+# <http://github.com/atelierspierrot/dev-tools>
 # <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
-# License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
-# Sources <http://github.com/atelierspierrot/dev-tools>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # action for Dev-Tools
 #
@@ -40,7 +52,7 @@ fi
 
 # options
 OPTIND=1
-while getopts ":${OPTIONS_ALLOWED}"; do
+while getopts ":${OPTIONS_ALLOWED}" OPTION; do
     OPTARG="${OPTARG#=}"
     case $OPTION in
         -) LONGOPTARG="`getlongoptionarg \"${OPTARG}\"`"
