a006089ae09a5ad9a23a7a8a277f7dd08507cfeb
Piero Wbmstr <me@e-piwi.fr>
2014-11-26 18:18:23 +0100

    wip on the test installer

--- dev/install-test.sh
+++ dev/install-test.sh
@@ -5,8 +5,7 @@ _INSTALLDIR=/usr/local
 [ -d /opt/local ] && _INSTALLDIR=/opt/local;
 
 # the default temporary directory
-_TMPDIR=/tmp
-[ -d $HOME/tmp ] && _TMPDIR=$HOME/tmp;
+_TMPDIR=$(mktemp -d)
 
 # the default user name & group
 _USERNAME=$(whoami)
@@ -38,6 +37,11 @@ if [ "$1" == '-x' ]; then
     echo "TempDir:      $_TMPDIR"
     echo "UserName:     $_USERNAME"
     echo "UserGroup:    $_USERGROUP"
+    echo "## install paths:"
+    echo "binaries:     $_INSTALLDIR/bin/"
+    echo "configs:      $_INSTALLDIR/etc/"
+    echo "libraris:     $_INSTALLDIR/lib/"
+    echo "manuals:      $_INSTALLDIR/man/man(X)/"
     exit 0
 fi
 
@@ -50,6 +54,7 @@ echo "Installing 'dev-tools' and dependencies in base path '$_INSTALLDIR'"
 ## the `piwi-bash-library`
 if ! $(which piwi-bash-library); then
     echo ">> installation of the 'piwi-bash-library' dependency ..."
+
     # download last master version archive
     echo "> downloading lastest master version archive ..."
     wget -q --no-check-certificate https://github.com/atelierspierrot/piwi-bash-library/archive/master.tar.gz 1>&/dev/null
@@ -61,14 +66,17 @@ if ! $(which piwi-bash-library); then
     echo "> installing the 'piwi-bash-library.sh' script in '$_INSTALLDIR/bin' ..."
     install -CD -o $_USERNAME -g $_USERGROUP -m 0655 piwi-bash-library-*/src/piwi-bash-library.sh $_INSTALLDIR/bin/piwi-bash-library.sh
     # symlink it to simple `piwi-bash-library`
-    ( cd $_INSTALLDIR/bin/ && ln -s piwi-bash-library.sh piwi-bash-library );
+    [ ! -f $_INSTALLDIR/bin/piwi-bash-library ] &&
+        ( cd $_INSTALLDIR/bin/ && ln -s piwi-bash-library.sh piwi-bash-library );
 
     # copy the piwi-bash-library manpage in manpages section 3
     echo "> installing the piwi-bash-library manpage to section 3 ..."
     install -CD -o $_USERNAME -g $_USERGROUP -m 0644 piwi-bash-library-*/src/piwi-bash-library.man $_INSTALLDIR/man/man3/piwi-bash-library.sh.3
     # gzip it
+    [ -f $_INSTALLDIR/man/man3/piwi-bash-library.sh.3.gz ] && rm -f $_INSTALLDIR/man/man3/piwi-bash-library.sh.3.gz;
     gzip $_INSTALLDIR/man/man3/piwi-bash-library.sh.3
     # symlink it to simple `devtools`
+    [ -f $_INSTALLDIR/man/man3/piwi-bash-library.3.gz ] && rm -f $_INSTALLDIR/man/man3/piwi-bash-library.3.gz;
     ( cd $_INSTALLDIR/man/man3/ && ln -s piwi-bash-library.sh.3.gz piwi-bash-library.3.gz );
 
     echo "_ ok"
@@ -90,7 +98,8 @@ tar -xf master
 echo "> installing the 'devtools.sh' command in '$_INSTALLDIR/bin' ..."
 install -CD -o $_USERNAME -g $_USERGROUP -m 0655 dev-tools-*/devtools.sh $_INSTALLDIR/bin/devtools.sh
 # symlink it to simple `devtools`
-( cd $_INSTALLDIR/bin/ && ln -s devtools.sh devtools );
+[ ! -f $_INSTALLDIR/bin/devtools ] &&
+    ( cd $_INSTALLDIR/bin/ && ln -s devtools.sh devtools );
 
 # install each action in "devtools-actions/" as "devtools-action.sh"
 echo "> installing the 'devtools-actions' in '$_INSTALLDIR/bin' ..."
@@ -107,13 +116,22 @@ install -CD -o $_USERNAME -g $_USERGROUP -m 0644 dev-tools-*/devtools.conf $_INS
 echo "> installing the piwi-bash-library manpage to section 3 ..."
 install -CD -o $_USERNAME -g $_USERGROUP -m 0644 dev-tools-*/devtools.man $_INSTALLDIR/man/man3/devtools.sh.3
 # gzip it
+[ -f $_INSTALLDIR/man/man3/devtools.sh.3.gz ] && rm -f $_INSTALLDIR/man/man3/devtools.sh.3.gz;
 gzip $_INSTALLDIR/man/man3/devtools.sh.3
 # symlink it to simple `devtools`
+[ -f $_INSTALLDIR/man/man3/devtools.3.gz ] && rm -f $_INSTALLDIR/man/man3/devtools.3.gz;
 ( cd $_INSTALLDIR/man/man3/ && ln -s devtools.sh.3.gz devtools.3.gz );
 
 # update the manpages DB
 echo "> updating manual pages DB ..."
-makewathis
+if [ `which makewhatis` ]; then
+    makewathis 1>&/dev/null
+elif [ `which mandb` ]; then
+    mandb 1>&/dev/null
+fi
+
+# cleanup
+rm -rf $_TMPDIR
 
 # done
 echo "_ ok : the 'dev-tools' are now installed in '$_INSTALLDIR'"
