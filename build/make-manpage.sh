967efd0d567e3e157e394443e18db295fa54d4f4
piwi <me@e-piwi.fr>
2014-12-20 23:54:29 +0100

    new builders shell scripts

---
+++ build/make-manpage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$(pwd)/build" == "$(dirname "$(realpath ${BASH_SOURCE[0]})")" ]
+then
+    source "$(dirname $0)/_settings.sh";
+else
+    echo "!! you must run shell builders from package's root directory !!"
+    exit 1
+fi
+
+if [ ! -f "$MDE_BIN" ]; then
+    echo "!! '${MDE_BIN}' not found!"
+    echo "run 'git submodule init / update'"
+    exit 1
+fi
+
+echo "> ${MDE_BIN} -f man -o '$MAN_FILE' '$MDMAN_FILE'"
+"${MDE_BIN}" -f man -o "$MAN_FILE" "$MDMAN_FILE" \
+    && echo "manpage generated in '${MDMAN_FILE}'" \
+    || echo "an error occurred!" ;
+
+exit 0
